0ffd82770ff69b7cc45a9a9054c650b881b2b028
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-10-21 16:45:30 -0400

    commit

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ var canvas = new fabric.Canvas('myCanvas');
 
 function new_image()
 {
-	
+	fabric.Image.fromUrl('BirthdayImage.jpg', function(Img){
+        block_image_object = Img;
+
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(150);
+        block_image_object.set({
+            top: 0,
+            left: 0,
+        })
+        canvas.add(block_image_object);
+    });
 	
 }
 
 function playSound(){
-	
+    x.play();	
 }
